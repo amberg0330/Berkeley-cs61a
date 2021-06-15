63e5f511b6d41623a5ad49c9dca1b7a970cb6def
Amber G <amber.g0330@gmail.com>
2021-06-14 22:13:50 -0700

    Update 08.py

--- Projects/Hog/tests/08.py
+++ Projects/Hog/tests/08.py
@@ -39,7 +39,7 @@ test = {
           >>> averaged_dice = make_averaged(dice, 1000)
           >>> # Average of calling dice 1000 times
           >>> averaged_dice()
-          ae54f398e6c98b4c11197ca202bbf4fb
+          3.75
           # locked
           """,
           'hidden': False,
@@ -52,7 +52,8 @@ test = {
           >>> # Average of calling roll_dice 1000 times
           >>> # Enter a float (e.g. 1.0) instead of an integer
           >>> averaged_roll_dice(2, dice)
-          0381158de1e7a3a31f3fcfeb3944e0dc
+          6.0 
+          ## why? 
           # locked
           """,
           'hidden': False,
